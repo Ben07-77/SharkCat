5910a1b71855585001582ea77d23ab3169593d14
ben <gamershubx902@gmail.com>
2024-11-07 09:30:36 +0100

    Initial commit

--- js/claim.js
+++ js/claim.js
@@ -36,24 +36,3 @@ darkModeButton.addEventListener('click', function () {
 });
 
 
-
-document.addEventListener('DOMContentLoaded', () => {
-    const sendButton = document.getElementById('sendButton');
-    sendButton.addEventListener('click', sendEmail);
-});
-
-function sendEmail() {
-    const emailInput = document.getElementById('email');
-    const messageInput = document.getElementById('message');
-
-    const recipientEmail = emailInput.value;
-    const message = messageInput.value;
-
-    // Prepare the email link
-    const subject = 'Check out this message!';
-    const body = encodeURIComponent(message);
-    const mailtoLink = `mailto:${recipientEmail}?subject=${subject}&body=${body}`;
-
-    // Open the email client
-    window.location.href = mailtoLink;
-}
